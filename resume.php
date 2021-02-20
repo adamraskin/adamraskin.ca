4e3d9b7c1d92ca11b90ec2f84a8cec3c2b6c2457
Adam Raskin <adamraskin25@gmail.com>
2021-02-20 12:28:01 -0500

    resume sizing

--- resume.php
+++ resume.php
@@ -10,9 +10,7 @@
       <h1>
         Resume
       </h1>
-	  <iframe src="/resources/resume.pdf">
-	  	failed to load pdf.
-	  </iframe>
+	  <iframe src="/resources/resume.pdf" width="100%" height="100%"></iframe>
     </center>
 	</body>
 </html>
